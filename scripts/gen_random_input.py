4b93a4ad54c990469f298dac9eb14d77082c81ea
Philippe Voinov <philippevoinov@gmail.com>
2020-10-31 13:10:31 +0100

    Generate pairs of inputs (independently or by removing elements from the first file)

--- scripts/gen_random_input.py
+++ scripts/gen_random_input.py
@@ -1,4 +1,4 @@
-from typing import Iterable, Literal, Optional
+from typing import Dict, Iterable, Literal, Optional
 import numpy as np
 from pathlib import Path
 from shutil import rmtree
@@ -9,6 +9,9 @@ def generate_input(
     distribution: Literal["uniform", "zipf"],
     values_range: Optional[int] = None,
 ):
+    if values_range is None:
+        values_range = length
+
     if distribution == "uniform":
         weights = np.ones(values_range)
     elif distribution == "zipf":
@@ -20,18 +23,50 @@ def generate_input(
     return np.random.choice(values_range, size=length, replace=True, p=weights)
 
 
-def write_as_diff_input(values: Iterable[int], path: Path):
-    with path.open("w", encoding="utf8") as f:
-        for v in values:
-            f.write(f"{v}\n")
+def generate_input_pair(
+    strategy: Literal["independent", "remove"],
+    length_1: int,
+    change_strength: float,  # 0 - no changes, 1 - many changes
+    distribution: Literal["uniform", "zipf"],
+):
+    if strategy == "independent":
+        assert change_strength == 1
+        return tuple(generate_input(length_1, distribution) for _ in range(2))
+
+    values_1 = generate_input(length_1, distribution)
+    assert len(values_1) == length_1
+
+    if strategy == "remove":
+        return (values_1, values_1[np.random.rand(length_1) > change_strength])
+
+
+def test_case_name_from_config(config: Dict):
+    parts = [
+        "random",
+        config["strategy"],
+        config["length_1"],
+        format(config["change_strength"], ".2f"),
+        config["distribution"],
+    ]
+    return "_".join(str(p) for p in parts)
 
 
 if __name__ == "__main__":
     all_test_cases_dir = Path(__file__).parent / "../test_cases"
 
-    test_case_dir = all_test_cases_dir / "temp"
-    rmtree(test_case_dir, ignore_errors=True)
-    test_case_dir.mkdir()
+    config = {
+        "strategy": "remove",
+        "length_1": 30,
+        "change_strength": 0.8,
+        "distribution": "zipf",
+    }
 
-    values = generate_input(30, "zipf")
-    write_as_diff_input(values, test_case_dir / "in_1.txt")
+    test_case_dir = all_test_cases_dir / test_case_name_from_config(config)
+    test_case_dir.mkdir(exist_ok=True)
+    for path in test_case_dir.glob("in_*.txt"):
+        path.unlink()
+
+    for i, values in enumerate(generate_input_pair(**config)):
+        with (test_case_dir / f"in_{i + 1}.txt").open("w", encoding="utf8") as f:
+            for v in values:
+                f.write(f"{v}\n")
