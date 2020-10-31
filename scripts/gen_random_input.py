679e63019de73edb6cf04ce3d1183b5749db3f50
Philippe Voinov <philippevoinov@gmail.com>
2020-10-31 16:55:02 +0100

    Support for chunky removals

--- scripts/gen_random_input.py
+++ scripts/gen_random_input.py
@@ -40,14 +40,73 @@ def random_interleaving(a: Sequence[int], b: Sequence[int]):
     return output
 
 
+def random_positive_integers_to_sum(sum: int, count: int):
+    assert 1 <= count <= sum
+    cuts = np.random.choice(sum, size=count, replace=False)
+    cuts = np.append(cuts, [sum])
+    cuts.sort()
+    cuts[0] = 0
+    assert len(np.unique(cuts)) == len(cuts)
+    values = cuts[1:] - cuts[:-1]
+    assert (values > 0).all()
+    assert values.sum() == sum
+    return values
+
+
+def random_chunky_mask(size: int, p_true: float, chunkiness: float):
+    assert 0 <= chunkiness <= 1
+    # HACK This is an attempt at making the chunkiness of the result less dependent on p_true.
+    chunkiness = min(
+        chunkiness, math.sqrt(chunkiness ** 2 + p_true ** 2) / math.sqrt(2)
+    )
+    assert 0 <= chunkiness <= 1
+
+    base_mask = np.random.rand(size) < p_true
+    chunk_count = int(math.floor(base_mask.sum() ** (1 - chunkiness)))
+    print("DEBUG", chunk_count)
+    chunk_sizes = random_positive_integers_to_sum(base_mask.sum(), chunk_count)
+
+    chunk_ranges = []
+    cur_size, cur_start, cur_chunk = 0, 0, 0
+    for i in range(size):
+        assert cur_start + cur_size <= i
+        if cur_chunk == chunk_count:
+            break
+        if not base_mask[i]:
+            if cur_size == 0:
+                cur_start = i + 1
+            continue
+        cur_size += 1
+        if cur_size == chunk_sizes[cur_chunk]:
+            chunk_ranges.append((cur_start, i + 1))
+            cur_chunk += 1
+            cur_size = 0
+            cur_start = i + 1
+    assert len(chunk_ranges) == chunk_count
+
+    chunky_mask = np.zeros(size, dtype=bool)
+    for (min_start, max_end), chunk_size in zip(chunk_ranges, chunk_sizes):
+        wiggle_room = (max_end - min_start) - chunk_size
+        assert wiggle_room >= 0
+        start = min_start + random.randint(0, wiggle_room)
+        end = start + chunk_size
+        chunky_mask[start:end] = True
+    assert base_mask.sum() == chunky_mask.sum()
+    return chunky_mask
+
+
 def generate_input_pair(
     length_1: int,
     strategy: Literal["independent", "remove", "add", "addremove"],
     change_strength: float,  # 0 - no changes, 1 - many changes
+    chunkiness: float,  # 0 - individual random changes, 1 - all changes consecutive
     distribution: Literal["uniform", "zipf"],
 ):
+    assert 0 < change_strength <= 1
+
     if strategy == "independent":
         assert change_strength == 1
+        assert chunkiness == 0
         return tuple(generate_input_1(length_1, distribution) for _ in range(2))
 
     def generate_input_2(values_1, strategy, change_strength):
@@ -55,15 +114,15 @@ def generate_input_pair(
         length_1 = len(values_1)
 
         if strategy == "remove":
-            return values_1[np.random.rand(length_1) > change_strength]
+            return values_1[~random_chunky_mask(length_1, change_strength, chunkiness)]
         elif strategy == "add":
+            assert chunkiness == 0
             addition_count = int(math.floor(length_1 * change_strength))
             added_values = generate_input_1(
                 addition_count, distribution, values_range=length_1
             )
             return random_interleaving(values_1, added_values)
         elif strategy == "addremove":
-            assert change_strength > 0
             values_with_added = generate_input_2(values_1, "add", change_strength)
             # roughly keep original length
             remove_change_strength = 1 - (1 / (1 + change_strength))
@@ -80,10 +139,11 @@ def test_case_name_from_config(config: Dict):
     parts = [
         "temp",
         "random",
-        config["strategy"],
-        config["length_1"],
-        format(config["change_strength"], ".2f"),
-        config["distribution"],
+        # config["strategy"],
+        # config["length_1"],
+        # format(config["change_strength"], ".2f"),
+        # format(config["chunkiness"], ".2f"),
+        # config["distribution"],
     ]
     return "_".join(str(p) for p in parts)
 
@@ -105,9 +165,10 @@ def generate_and_save_test_case(config: Dict):
 if __name__ == "__main__":
     generate_and_save_test_case(
         {
-            "strategy": "addremove",
-            "length_1": 30,
-            "change_strength": 0.1,
+            "strategy": "remove",
+            "length_1": 70,
+            "change_strength": 0.4,
+            "chunkiness": 0.5,
             "distribution": "zipf",
         }
     )
