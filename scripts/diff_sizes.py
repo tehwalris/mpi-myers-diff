dca7a4be9d0173280babb33faf17b51a4afa1729
Philippe Voinov <philippevoinov@gmail.com>
2020-11-06 14:17:51 +0100

    Refactor diff script to allow reuse

--- scripts/diff_sizes.py
+++ scripts/diff_sizes.py
@@ -3,48 +3,61 @@ import sys
 import subprocess
 from subprocess import check_call
 from termcolor import colored
+import traceback
+from pathlib import Path
 
-test_case_folder = "../test_cases/"
+test_case_folder = Path(__file__).parent / "../test_cases/"
 diff_filename = "diff_size.txt"
 diff_command = 'diff --minimal in_1.txt in_2.txt | egrep -c "^[<>]" > diff_size.txt'
-always_create_diff = False
-
 help_msg = """
 Input Arguments
 default:  generate missing diffs
 all:      generate all diffs again
 """
-if len(sys.argv) > 1:
-    arg = sys.argv[1]
-    if arg.lower() == "all":
-        always_create_diff = True
-    else:
-        print(help_msg)
-        exit(1)
 
 
-# go through test_case folders
-counter = 0
-print(f"{'Folder':<18}Edit Distance")
-for folder in os.scandir(test_case_folder):
-    if folder.is_dir():
+def update_test_case_diff(test_case_folder_path, always_create_diff=True):
+    diff_path = test_case_folder_path + "/" + diff_filename
+    diff_exists = os.path.isfile(diff_path)
 
-        diff_path = folder.path + "/" + diff_filename
-        diff_exists = os.path.isfile(diff_path)
+    if diff_exists and not always_create_diff:
+        return None
 
-        # create diff file
-        if always_create_diff or not diff_exists:
-            try:
-                result = check_call(diff_command, shell=True, cwd=folder.path)
-                f = open(diff_path, "r")
-                result = f.read().rstrip()
-                f.close()
-                counter += 1
-                print(f"{folder.name:<18}{result}")
+    try:
+        check_call(diff_command, shell=True, cwd=folder.path)
+    except subprocess.CalledProcessError as e:
+        os.remove(diff_path)
+        raise e
 
-            # delete incorrect file and show error
-            except subprocess.CalledProcessError as e:
-                os.remove(diff_path)
-                print(f"{folder.name:<18}{colored('Failed', 'red')} to generate diff")
+    with open(diff_path, "r") as f:
+        return int(f.read().rstrip())
 
-print(f"\nGenerated {counter} Files")
+
+if __name__ == "__main__":
+    always_create_diff = False
+    if len(sys.argv) > 1:
+        arg = sys.argv[1]
+        if arg.lower() == "all":
+            always_create_diff = True
+        else:
+            print(help_msg)
+            exit(1)
+
+    counter = 0
+    print(f"{'Folder':<50}Edit Distance")
+    for folder in os.scandir(test_case_folder):
+        if not folder.is_dir():
+            continue
+
+        try:
+            result = update_test_case_diff(folder.path, always_create_diff)
+        except Exception:
+            traceback.print_exc()
+            print(f"{folder.name:<50}{colored('Failed', 'red')} to generate diff")
+            continue
+
+        if result is not None:
+            counter += 1
+            print(f"{folder.name:<50}{result}")
+
+    print(f"\nGenerated {counter} Files")
