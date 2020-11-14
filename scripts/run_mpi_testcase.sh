49f03576c299829856ea23d08b9afaaeedaf246c
Pascal Maillard <pascalm@student.ethz.ch>
2020-11-14 15:59:25 +0100

    add mpi run script

---
+++ scripts/run_mpi_testcase.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+default_np=4
+executable_name=a.out
+
+DIR="$(dirname "$(readlink -f "$BASH_SOURCE")")"
+
+if [ "$#" -eq  "0" ]
+   then	# no args
+     echo "1st arg     		the name of the test folder"
+     echo "2nd arg (optional) 	is the number of processes"
+     exit 1
+ elif [ "$#" -eq  "1" ]
+   then
+     mpiexec -np $default_np $DIR/../$executable_name $DIR/../test_cases/$1/in_1.txt  $DIR/../test_cases/$1/in_2.txt 
+ else
+     mpiexec -np $2 $DIR/../$executable_name $DIR/../test_cases/$1/in_1.txt  $DIR/../test_cases/$1/in_2.txt 
+fi
