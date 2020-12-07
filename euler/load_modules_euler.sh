8c3feb2f7a5210a2c14517de1eb34f0d0e5c43d7
Pascal Maillard <pascalm@student.ethz.ch>
2020-12-07 16:46:05 +0100

    update euler modules

--- euler/load_modules_euler.sh
+++ euler/load_modules_euler.sh
@@ -5,4 +5,5 @@ echo "-> Loading modules required for build"
 module list &> /dev/null || source /cluster/apps/modules/init/bash
 
 module purge
-module load legacy new gcc/4.8.2 open_mpi/1.6.5 python/3.6.0
+module load legacy new gcc/6.3.0 open_mpi/3.0.0 python/3.7.1
+module list
