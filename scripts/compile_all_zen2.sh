ca5e529e7d4374e0d0052742d43151545f5fd55d
lengel <lengel@student.ethz.ch>
2020-12-17 10:20:51 +0100

    updated readme and zen2 binary folder

--- scripts/compile_all_zen2.sh
+++ scripts/compile_all_zen2.sh
@@ -6,47 +6,47 @@ cd $DIR/..
 CFLAGS="-std=c++17 -O3 -Wall -DNDEBUG -march=znver2 -ffast-math"
 
 printf "\nCompiling MPI Master:\n"
-mpic++ src/main.cpp -o bin/own-diff-mpi-master.out $CFLAGS
+mpic++ src/main.cpp -o bin-euler/own-diff-mpi-master.out $CFLAGS
 
 printf "\nCompiling MPI Master with SIMD:\n"
-mpic++ src/main.cpp -o bin/own-diff-mpi-master-simd.out -DSIMD $CFLAGS
+mpic++ src/main.cpp -o bin-euler/own-diff-mpi-master-simd.out -DSIMD $CFLAGS
 
 printf "\nCompiling MPI No Master (FastStorage):\n"
-mpic++ src/no_master_synch.cpp -o bin/own-diff-mpi-no-master.out $CFLAGS
+mpic++ src/no_master_synch.cpp -o bin-euler/own-diff-mpi-no-master.out $CFLAGS
 
 printf "\nCompiling MPI No Master (FastStorage) with SIMD:\n"
-mpic++ src/no_master_synch.cpp -o bin/own-diff-mpi-no-master-simd.out -DSIMD $CFLAGS
+mpic++ src/no_master_synch.cpp -o bin-euler/own-diff-mpi-no-master-simd.out -DSIMD $CFLAGS
 
 printf "\nCompiling MPI No Master (FrontierStorage):\n"
-mpic++ src/no_master_synch.cpp -o bin/own-diff-mpi-no-master-frontier.out -DFRONTIER_STORAGE $CFLAGS
+mpic++ src/no_master_synch.cpp -o bin-euler/own-diff-mpi-no-master-frontier.out -DFRONTIER_STORAGE $CFLAGS
 
 printf "\nCompiling MPI No Master (FrontierStorage) with SIMD:\n"
-mpic++ src/no_master_synch.cpp -o bin/own-diff-mpi-no-master-frontier-simd.out -DFRONTIER_STORAGE -DSIMD $CFLAGS
+mpic++ src/no_master_synch.cpp -o bin-euler/own-diff-mpi-no-master-frontier-simd.out -DFRONTIER_STORAGE -DSIMD $CFLAGS
 
 printf "\nCompiling MPI Priority (FastStorage):\n"
-mpic++ src/priority/main.cpp -o bin/own-diff-mpi-priority.out $CFLAGS
+mpic++ src/priority/main.cpp -o bin-euler/own-diff-mpi-priority.out $CFLAGS
 
 printf "\nCompiling MPI Priority (FastStorage) with SIMD:\n"
-mpic++ src/priority/main.cpp -o bin/own-diff-mpi-priority-simd.out -DSIMD $CFLAGS
+mpic++ src/priority/main.cpp -o bin-euler/own-diff-mpi-priority-simd.out -DSIMD $CFLAGS
 
 printf "\nCompiling MPI Priority (FrontierStorage):\n"
-mpic++ src/priority/main.cpp -o bin/own-diff-mpi-priority-frontier.out -DFRONTIER_STORAGE $CFLAGS
+mpic++ src/priority/main.cpp -o bin-euler/own-diff-mpi-priority-frontier.out -DFRONTIER_STORAGE $CFLAGS
 
 printf "\nCompiling MPI Priority (FrontierStorage) with SIMD:\n"
-mpic++ src/priority/main.cpp -o bin/own-diff-mpi-priority-frontier-simd.out -DFRONTIER_STORAGE -DSIMD $CFLAGS
+mpic++ src/priority/main.cpp -o bin-euler/own-diff-mpi-priority-frontier-simd.out -DFRONTIER_STORAGE -DSIMD $CFLAGS
 
 printf "\nCompiling Sequential Fast Snakes:\n"
-g++ src/sequential_all_snakes_fast_lookup.cpp -o bin/own-diff-sequential-fast-snakes.out $CFLAGS
+g++ src/sequential_all_snakes_fast_lookup.cpp -o bin-euler/own-diff-sequential-fast-snakes.out $CFLAGS
 
 printf "\nCompiling Sequential (FastStorage):\n"
-g++ src/sequential.cpp -o bin/own-diff-sequential.out $CFLAGS
+g++ src/sequential.cpp -o bin-euler/own-diff-sequential.out $CFLAGS
 
 printf "\nCompiling Sequential (FastStorage) with SIMD:\n"
-g++ src/sequential.cpp -o bin/own-diff-sequential-simd.out -DSIMD $CFLAGS
+g++ src/sequential.cpp -o bin-euler/own-diff-sequential-simd.out -DSIMD $CFLAGS
 
 printf "\nCompiling Sequential (FrontierStorage):\n"
-g++ src/sequential.cpp -o bin/own-diff-sequential-frontier.out -DFRONTIER_STORAGE $CFLAGS
+g++ src/sequential.cpp -o bin-euler/own-diff-sequential-frontier.out -DFRONTIER_STORAGE $CFLAGS
 
 printf "\nCompiling Sequential (FrontierStorage) with SIMD:\n"
-g++ src/sequential.cpp -o bin/own-diff-sequential-frontier-simd.out -DFRONTIER_STORAGE -DSIMD $CFLAGS
+g++ src/sequential.cpp -o bin-euler/own-diff-sequential-frontier-simd.out -DFRONTIER_STORAGE -DSIMD $CFLAGS
 
