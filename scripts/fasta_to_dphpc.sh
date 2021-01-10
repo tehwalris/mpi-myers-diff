dad891eceff2112688c36119b06739dfcaa1fb85
Josua Cantieni <josuac@student.ethz.ch>
2021-01-10 18:06:47 +0100

    Added 1.8 Mio DNA testcase

--- scripts/fasta_to_dphpc.sh
+++ scripts/fasta_to_dphpc.sh
@@ -6,4 +6,4 @@ then
     exit 1
 fi
 
-sed "/^>/d" $1 | tr -d "\n" | sed "s/N//g; s/A/1\\n/g; s/C/2\\n/g; s/G/3\\n/g; s/T/4\\n/g"
+sed "/^>/d" $1 | tr -d "\n" | tr -d "\r" | sed "s/N//g; s/A/1\\n/g; s/C/2\\n/g; s/G/3\\n/g; s/T/4\\n/g"
